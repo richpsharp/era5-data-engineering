2f8ca77e0415a899826088ea1b0af9aba73ac7ca
Sambadi Majumder <83839244+SamMajumder@users.noreply.github.com>
2024-09-30 15:16:08 -0400

    making small change in to the resources YAML (#56)
    * making small change in to the resources YAML
    
    source to bronze (on demand = 1)
    
    * Making edits to the staging to bronze comments
    
    ---------
    
    Co-authored-by: SamMajumder <SamMajumder>

--- era5-daily/01_Staging_to_Bronze/staging_to_bronze/src/01_Staging_to_Bronze.py
+++ era5-daily/01_Staging_to_Bronze/staging_to_bronze/src/01_Staging_to_Bronze.py
@@ -1,6 +1,5 @@
 # Databricks notebook source
-# MAGIC
-# MAGIC %pip install numpy==1.26.4 ## please use this version of numpy ## DO NOT USE THE LATEST VERSION
+# MAGIC %pip install numpy==1.26.4 #### please use this version of numpy ## DO NOT USE THE LATEST VERSION
 # MAGIC %pip install xarray
 # MAGIC %pip install netCDF4 h5netcdf
 
