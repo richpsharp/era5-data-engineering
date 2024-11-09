ae95a0649fa4e7e676a98a4ce97350c5332f9e3f
Sambadi Majumder <83839244+SamMajumder@users.noreply.github.com>
2024-11-09 15:09:59 -0600

    Update 01_Source_to_Staging.py

--- era5-daily/Combined_Run/src/01_Source_to_Staging/01_Source_to_Staging.py
+++ era5-daily/Combined_Run/src/01_Source_to_Staging/01_Source_to_Staging.py
@@ -273,8 +273,8 @@ elif workspace_url == staging_workspace_url:
     target_folder = '/Volumes/era5-daily-data/bronze_staging/era5_gwsc_staging_folder'
     table_name="`era5-daily-data`.bronze_staging.era5_inventory_table"
     
-    start_date = '1968-06-08'
-    end_date = '1984-12-31'
+    start_date = '1984-01-09'
+    end_date = '2024-10-31'
     source_folder = '/Volumes/aer-processed/era5/daily_summary'
     prefix = 'reanalysis-era5-sfc-daily-'
     date_pattern = '%Y-%m-%d'
