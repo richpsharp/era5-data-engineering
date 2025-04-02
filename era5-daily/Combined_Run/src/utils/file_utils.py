d2546d7347e1dd25e66167fe47b308b3d001bb57
Richard P Sharp Jr. <richpsharp@gmail.com>
2025-04-02 15:39:52 -0700

    writing to a tempfile

--- era5-daily/Combined_Run/src/utils/file_utils.py
+++ era5-daily/Combined_Run/src/utils/file_utils.py
@@ -1,5 +1,7 @@
 """Module for supporting file operations on Databricks."""
 
+import tempfile
+import os
 import io
 import hashlib
 import logging
@@ -7,6 +9,15 @@ import logging
 import xarray as xr
 
 LOGGER = logging.getLogger(__name__)
+TEMPFILE_ROOT = "/local_disk0"
+
+if not os.path.exists(TEMPFILE_ROOT):
+    LOGGER.warning(
+        f"{TEMPFILE_ROOT} did not exist which is a fast nvme mount on "
+        f"databricks, this means netcdf validity checks will be slower by "
+        "writing to /tmp"
+    )
+    TEMPFILE_ROOT = "/tmp"
 
 
 def is_netcdf_file_valid(file_binary, source_file_path):
@@ -20,8 +31,12 @@ def is_netcdf_file_valid(file_binary, source_file_path):
         bool: True if the file is valid, False if corrupt.
     """
     try:
-        with xr.open_dataset(io.BytesIO(file_binary)) as ds:
-            # this will raise an exception if it's wrong
+        with tempfile.NamedTemporaryFile(suffix=".nc", delete=False) as tmp:
+            tmp.write(file_binary)
+            tmp.flush()
+            temp_filename = tmp.name
+        with xr.open_dataset(temp_filename, engine="netcdf4") as ds:
+            # this will raise an exception if it can't open it as a netcdf4
             ds.load()
         return True
     except Exception:
@@ -29,6 +44,8 @@ def is_netcdf_file_valid(file_binary, source_file_path):
             f"something bad happend on when trying to read {source_file_path}"
         )
         return False
+    finally:
+        os.remove(temp_filename)
 
 
 def copy_file_to_mem(source_path):
