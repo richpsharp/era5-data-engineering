d8b8f8893a6953b11ffcb1c525598e599ec7a031
Richard P Sharp Jr. <richpsharp@gmail.com>
2025-04-16 11:09:13 -0700

    rename with bronze and inject the branch name into the dab

--- era5-daily/Combined_Run/src/config.py
+++ era5-daily/Combined_Run/src/config.py
@@ -5,6 +5,6 @@ ERA5_INVENTORY_TABLE_NAME = "era5_gwsc_staging_inventory_table_v2"
 ERA5_SOURCE_VOLUME_PATH = "/Volumes/aer-processed/era5"
 ERA5_STAGING_VOLUME_ID = "era5_gwsc_staging_folder_v2"
 ERA5_SOURCE_VOLUME_FQDN = "aer-processed.era5.daily_summary"
-# This is the nvme location if you use an i3.* node
+# This is the nvme location if you use an i4.* node
 LOCAL_EPHEMERAL_PATH = "/local_disk0/workspace"
 DEFAULT_LOCAL_CATALOG_FQDN = "sandbox.sandbox"
