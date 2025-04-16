e3752ad3e312806bce18d99b7a417feb82b354ec
Richard P Sharp Jr. <richpsharp@gmail.com>
2025-04-16 12:37:52 -0700

    re #115 changing names to be conformany to naming convention

--- era5-daily/Combined_Run/src/config.py
+++ era5-daily/Combined_Run/src/config.py
@@ -1,9 +1,9 @@
 """Global configurations for the era5 DAB."""
 
 ERA5_INVENTORY_TABLE_DEFINITION_PATH = "./era5_table_definitions.yml"
-ERA5_INVENTORY_TABLE_NAME = "era5_gwsc_staging_inventory_table_v2"
+ERA5_INVENTORY_TABLE_NAME = "era5_staging_inventory_bronze"
 ERA5_SOURCE_VOLUME_PATH = "/Volumes/aer-processed/era5"
-ERA5_STAGING_VOLUME_ID = "era5_gwsc_staging_folder_v2"
+ERA5_STAGING_VOLUME_ID = "era5_staging_folder_bronze"
 ERA5_SOURCE_VOLUME_FQDN = "aer-processed.era5.daily_summary"
 # This is the nvme location if you use an i4.* node
 LOCAL_EPHEMERAL_PATH = "/local_disk0/workspace"
